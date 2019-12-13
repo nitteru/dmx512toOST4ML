d8c3b75c7e1bf28228ef4ffb3a96070cc01f02e5
Teruyuki Nishi <nitteru@gmail.com>
2019-12-14 00:10:08 +0900

    * Add MCC Code

---
+++ dmx512toOST4ML.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=dmx512toOST4ML.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/dmx512toOST4ML.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=dmx512toost4ml.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/dmx512toost4ml.x.tar
